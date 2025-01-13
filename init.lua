dc3a34bc22823ce57007a8b5a6c0efe27a583243
Mohammed Moiz Pasha <moizpasha17@gmail.com>
2025-01-13 21:21:24 +0530

    added <Esc> remap

--- init.lua
+++ init.lua
@@ -189,6 +189,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Remap jk and kj to Escape in insert mode
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true, silent = true })
+
+-- Remap jk and kj to Escape in visual mode
+vim.api.nvim_set_keymap('v', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'kj', '<Esc>', { noremap = true, silent = true })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
